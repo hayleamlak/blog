ce3350173e66889445a42c71079a0abee8d3750c
hayleamlak <hayleamlaktesfaye12@gmail.com>
2025-10-21 14:33:59 +0300

    add a router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,8 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import BlogDetail from "./pages/BlogDetail";
-import CreatePost from "./pages/CreatePost"; // <-- import it
+import CreatePost from "./pages/CreatePost";
+import Profile from "./pages/Profile";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -15,8 +16,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/profile" element={<Profile />} />
           <Route path="/blog/:id" element={<BlogDetail />} />
-          <Route path="/create-post" element={<CreatePost />} /> {/* <-- add this */}
         </Routes>
       </div>
     </Router>
